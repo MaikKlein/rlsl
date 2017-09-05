6efeafa3623a531056577a157b121a29c339b2fc
Maik Klein <maikklein@googlemail.com>
2017-09-05 12:27:23 +0200

    working libstd

--- libstd/src/lib.rs
+++ libstd/src/lib.rs
@@ -4,6 +4,7 @@
 #![feature(core_intrinsics)]
 #![feature(unwind_attributes)]
 #![feature(core_panic)]
+#![feature(prelude_import)]
 
 #[macro_reexport(assert, assert_eq, assert_ne, debug_assert, debug_assert_eq,
 debug_assert_ne, unreachable, unimplemented, write, writeln, try)]
@@ -14,7 +15,18 @@ pub use core::marker;
 pub use core::ops;
 pub use core::fmt;
 pub use core::panicking;
+#[lang = "panic_fmt"]
 pub use core::panicking::panic_fmt;
+pub use core::result;
+pub use core::option;
+pub use core::cmp;
+pub use core::convert;
+pub use core::slice;
+pub use core::borrow;
+pub use core::mem;
+pub mod prelude;
+#[prelude_import]
+pub use prelude::v1::*;
 
-#[lang = "eh_personality"] pub extern fn eh_personality() {}
+/#[lang = "eh_personality"] pub extern fn eh_personality() {}
 
---
+++ libstd/src/prelude/v1.rs
@@ -0,0 +1,19 @@
+#[doc(no_inline)] pub use marker::{Copy, Send, Sized, Sync};
+#[doc(no_inline)] pub use ops::{Drop, Fn, FnMut, FnOnce};
+
+#[doc(no_inline)] pub use mem::drop;
+
+// Reexported types and traits
+//#[doc(no_inline)] pub use boxed::Box;
+//#[doc(no_inline)] pub use borrow::ToOwned;
+#[doc(no_inline)] pub use clone::Clone;
+#[doc(no_inline)] pub use cmp::{PartialEq, PartialOrd, Eq, Ord};
+#[doc(no_inline)] pub use convert::{AsRef, AsMut, Into, From};
+//#[doc(no_inline)] pub use default::Default;
+//#[doc(no_inline)] pub use iter::{Iterator, Extend, IntoIterator};
+//#[doc(no_inline)] pub use iter::{DoubleEndedIterator, ExactSizeIterator};
+#[doc(no_inline)] pub use option::Option::{self, Some, None};
+#[doc(no_inline)] pub use result::Result::{self, Ok, Err};
+//#[doc(no_inline)] pub use slice::SliceConcatExt;
+//#[doc(no_inline)] pub use string::{String, ToString};
+//#[doc(no_inline)] pub use vec::Vec;
