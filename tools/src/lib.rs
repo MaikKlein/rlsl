389d46e35e76962251d474fb81adb067d473b13b
maik <maik.klein@embark-studios.com>
2019-10-26 12:24:37 +0200

    Install prerequisites for rlsl

--- tools/src/lib.rs
+++ tools/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use std::fmt;
 use std::path::Path;
 use std::path::PathBuf;
@@ -8,7 +9,7 @@ pub enum Error {
     Rustup,
 }
 
-pub type ToolResult<T> = std::result::Result<T, Error>;
+pub type ToolResult<T> = std::result::Result<T, anyhow::Error>;
 
 impl std::error::Error for Error {}
 impl fmt::Display for Error {
@@ -44,6 +45,11 @@ pub fn check_rustup() -> ToolResult<()> {
     Ok(())
 }
 
+pub fn check_xargo() -> ToolResult<()> {
+    Command::new("xargo").output().map_err(|_| Error::Rustup)?;
+    Ok(())
+}
+
 pub fn find_toolchain_dir(version: &str) -> Option<PathBuf> {
     PathBuf::from(dirs::home_dir().unwrap())
         .join(".rustup/toolchains")
@@ -68,5 +74,85 @@ pub fn link_toolchain(name: &str, path: &Path) -> ToolResult<()> {
         .arg(format!("{}", path.display()))
         .status()
         .map_err(|_| Error::Rustup)?;
+    Ok(())
+}
+
+pub fn install_toolchain_src(version: &str) -> ToolResult<()> {
+    Command::new("rustup")
+        .arg("component")
+        .arg("add")
+        .arg("rust-src")
+        .arg("--toolchain")
+        .arg(format!("{}", version))
+        .status()
+        .map_err(|_| Error::Rustup)?;
+    Ok(())
+}
+
+pub fn build_custom_libstd() -> ToolResult<()> {
+    let src_path = PathBuf::from(dirs::home_dir().unwrap())
+        .join(".rustup/toolchains/rlsl/lib/rustlib/src/rust/src");
+
+    Command::new("rustup")
+        .env("XARGO_RUST_SRC", format!("{}", src_path.display()))
+        .env("RUSTFLAGS", "-Z always-encode-mir")
+        .arg("run")
+        .arg("rlsl")
+        .arg("xargo")
+        .arg("build")
+        .arg("--release")
+        .arg("--manifest-path")
+        .arg("libstd/Cargo.toml")
+        .status()
+        .map_err(|_| Error::Rustup)?;
+
+    Ok(())
+}
+
+pub fn install_custom_libstd() -> ToolResult<()> {
+    use std::fs;
+    let triple = platforms::guess_current().unwrap().target_triple;
+    let home_dir = dirs::home_dir().unwrap();
+    let path = home_dir.join(format!(".xargo/HOST/lib/rustlib/{}/lib", triple));
+
+    let find_rlib = |name: &str| -> Option<PathBuf> {
+        path.read_dir().ok()?.find_map(|entry| {
+            let file_path = entry.ok()?.path();
+            if file_path.extension()? != "rlib" {
+                return None;
+            }
+
+            let file_name = file_path.file_stem()?.to_str()?;
+            if file_name.starts_with(name) {
+                Some(file_path)
+            } else {
+                None
+            }
+        })
+    };
+    let libcore = find_rlib("libcore").context("Unable to find libcore")?;
+    let libcompiler_builtins =
+        find_rlib("libcompiler_builtins").context("Unable to find compiler builtins")?;
+
+    fs::copy(
+        "target/release/libstd.rlib",
+        home_dir.join(".rlsl/lib/libstd.rlib"),
+    )?;
+    fs::copy(libcore, home_dir.join(".rlsl/lib/libcore.rlib"))?;
+    fs::copy(
+        libcompiler_builtins,
+        home_dir.join(".rlsl/lib/libcompiler_builtins.rlib"),
+    )?;
+
+    Ok(())
+}
+
+pub fn create_rlsl_dirs() -> ToolResult<()> {
+    use std::fs;
+    let home_dir = dirs::home_dir().unwrap();
+
+    fs::create_dir_all(home_dir.join(".rlsl"))?;
+    fs::create_dir_all(home_dir.join(".rlsl/lib"))?;
+
     Ok(())
 }
--- tools/src/main.rs
+++ tools/src/main.rs
@@ -1,14 +1,28 @@
-use std::process::Command;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tools::check_rustup()?;
+    tools::check_xargo()?;
+
+    println!("Creating .rlsl at home dir");
+    tools::create_rlsl_dirs()?;
+
     let version = tools::compute_nightly_name();
+
     println!("Installing version {}", version);
     tools::install_nightly(&version)?;
+
     let toollchain_path =
         tools::find_toolchain_dir(&version).expect("Unable to find suitable toolchain");
-    println!("Linking toolchain");
-    tools::link_toolchain("rlsl", &toollchain_path)?;
-    println!("working");
 
+    println!("Linking toolchain rlsl to {}", version);
+    tools::link_toolchain("rlsl", &toollchain_path)?;
+
+    println!("Adding component rust-src to toolchain {}", version);
+    tools::install_toolchain_src(&version)?;
+
+    println!("Building custom libstd/libcore");
+    tools::build_custom_libstd();
+
+    println!("Installing libstd/libcore");
+    tools::install_custom_libstd();
     Ok(())
 }
